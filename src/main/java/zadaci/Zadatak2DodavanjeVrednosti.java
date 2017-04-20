d64c89561a0e3ef9dba860fadb007ec4c0731d28
Nenad <norlovic@gmail.com>
2017-04-20 20:03:34 +0200

    Kreirane klase za zadatke 2

--- src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
@@ -1,10 +1,32 @@
 package zadaci;
 
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+
 /**
  * Created by androiddevelopment on 20.4.17..
  */
 public class Zadatak2DodavanjeVrednosti {
     public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        // create our data-source for the database
+        try{
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
 
     }
 }
