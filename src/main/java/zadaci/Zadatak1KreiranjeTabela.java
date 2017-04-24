a94a764513f53474051c1dec560d0b20a3109cc9
Nikola Japundzic <Nikola Japundzic>
2017-04-24 23:32:49 +0200

    Uradjen zadatak kreiranje tabela

--- src/main/java/zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -2,6 +2,9 @@ package zadaci;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Knjiga;
+import model.Oblast;
 
 import java.io.IOException;
 
@@ -14,6 +17,13 @@ public class Zadatak1KreiranjeTabela {
         // create our data-source for the database
         try{
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
+
+            TableUtils.dropTable(connectionSource, Oblast.class, true);
+            TableUtils.dropTable(connectionSource, Knjiga.class, true);
+
+            TableUtils.createTable(connectionSource, Knjiga.class);
+            TableUtils.createTable(connectionSource, Oblast.class);
+
         }catch (Exception e) {
             e.printStackTrace();
         }finally {
